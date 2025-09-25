eddad368beb95fb737c230b206d9b08e1b77aee3
gramos22 <gabrielrmzf@gmail.com>
2025-09-25 16:50:25 -0300

    feat: adiciona constantes para salvar queries & models para analise de PR

--- code/src/domain/models.py
+++ code/src/domain/models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List
+from datetime import datetime
 
 @dataclass
 class Repository:
@@ -9,3 +10,22 @@ class Repository:
     stargazers: int
     url: str
     description: Optional[str] = None
+
+@dataclass
+class Review:
+    author: str
+    submitted_at: datetime
+
+@dataclass
+class PullRequest:
+    number: int
+    title: str
+    author: str
+    created_at: datetime
+    closed_at: datetime
+    merged: bool
+    additions: int
+    deletions: int
+    changed_files: int
+    reviews: List[Review]
+    url: str
